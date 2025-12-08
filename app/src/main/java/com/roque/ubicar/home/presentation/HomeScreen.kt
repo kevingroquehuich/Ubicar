dd470c6dac000ac420b79a40b76ad499a3763c18
Keving Hanz Roque Huich <keving.roque.huich@gmail.com>
2025-12-08 16:37:00 -0500

    Add Google Maps

--- app/src/main/java/com/roque/ubicar/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/roque/ubicar/home/presentation/HomeScreen.kt
@@ -1,11 +1,31 @@
 package com.roque.ubicar.home.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.roque.ubicar.home.presentation.components.HomeMap
 
 @Composable
 fun HomeScreen(
     viewmodel: HomeViewmodel = hiltViewModel()
 ) {
+
+    val state = viewmodel.state
+
+    Scaffold(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(it)
+        ) {
+            HomeMap(
+                currentLocation = null,
+                carLocation = null,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
 
 }
---
+++ app/src/main/java/com/roque/ubicar/home/presentation/components/HomeMap.kt
@@ -0,0 +1,29 @@
+package com.roque.ubicar.home.presentation.components
+
+import android.location.Location
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+
+
+@Composable
+fun HomeMap(
+    currentLocation: Location?,
+    carLocation: Location?,
+    modifier: Modifier = Modifier
+) {
+    GoogleMap(
+        modifier = modifier,
+        properties = MapProperties(
+            isMyLocationEnabled = true
+        )
+    ) {  }
+}
+
+@Preview
+@Composable
+fun HomeMapPreview() {
+    HomeMap(null, null)
+}
