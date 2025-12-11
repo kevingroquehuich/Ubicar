9dbb6bd828a7f28e1bba49f19ed660090c6417c6
Keving Hanz Roque Huich <keving.roque.huich@gmail.com>
2025-12-11 02:04:24 -0500

    Refactor the usecase to test

--- app/src/main/java/com/roque/ubicar/home/di/HomeModule.kt
+++ app/src/main/java/com/roque/ubicar/home/di/HomeModule.kt
@@ -2,6 +2,7 @@ package com.roque.ubicar.home.di
 
 import android.content.Context
 import androidx.room.Room
+import com.roque.ubicar.home.data.distance.DistanceCalculatorImpl
 import com.roque.ubicar.home.data.repository.LocationServiceImpl
 import com.roque.ubicar.home.data.local.UbicarDatabase
 import com.roque.ubicar.home.data.local.dao.HomeDao
@@ -9,6 +10,7 @@ import com.roque.ubicar.home.data.remote.GoogleDirectionsApi
 import com.roque.ubicar.home.data.repository.HomeRepositoryImpl
 import com.roque.ubicar.home.domain.HomeRepository
 import com.roque.ubicar.home.domain.LocationService
+import com.roque.ubicar.home.domain.distance.DistanceCalculator
 import com.roque.ubicar.home.domain.usecase.GetPathToCarUseCase
 import dagger.Module
 import dagger.Provides
@@ -73,7 +75,14 @@ object HomeModule {
 
     @Provides
     @Singleton
-    fun provideGetPathToCarUseCase(repository: HomeRepository): GetPathToCarUseCase =
-        GetPathToCarUseCase(repository)
+    fun provideDistanceCalculator(): DistanceCalculator = DistanceCalculatorImpl()
+
+    @Provides
+    @Singleton
+    fun provideGetPathToCarUseCase(
+        repository: HomeRepository,
+        distanceCalculator: DistanceCalculator
+    ): GetPathToCarUseCase =
+        GetPathToCarUseCase(repository, distanceCalculator)
 
 }
--- app/src/main/java/com/roque/ubicar/home/domain/usecase/GetPathToCarUseCase.kt
+++ app/src/main/java/com/roque/ubicar/home/domain/usecase/GetPathToCarUseCase.kt
@@ -1,14 +1,14 @@
 package com.roque.ubicar.home.domain.usecase
 
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.ktx.utils.isLocationOnPath
 import com.roque.ubicar.home.domain.HomeRepository
+import com.roque.ubicar.home.domain.distance.DistanceCalculator
 import com.roque.ubicar.home.domain.model.Location
 import com.roque.ubicar.home.domain.model.Route
 import kotlin.math.roundToInt
 
 class GetPathToCarUseCase(
-    private val repository: HomeRepository
+    private val repository: HomeRepository,
+    private val distanceCalculator: DistanceCalculator
 ) {
 
     companion object {
@@ -21,15 +21,11 @@ class GetPathToCarUseCase(
         route: Route
     ): Result<Route> {
         val closestIndex = getClosestLocationIndex(currentLocation, route.polylines)
-        val isOnRoute = route.polylines.map { LatLng(it.latitude, it.longitude) }.isLocationOnPath(
-            LatLng(currentLocation.latitude, currentLocation.longitude),
-            false,
-            MAX_METERS
-        )
+        val isOnRoute = distanceCalculator.isLocationOnPath(route.polylines, currentLocation, MAX_METERS)
 
         return if (isOnRoute) {
             val newPolylines = route.polylines.drop(closestIndex)
-            val distance = calculateDistanceBetweenLocations(currentLocation, newPolylines.last())
+            val distance = distanceCalculator.calculateDistanceBetweenLocations(currentLocation, newPolylines.last())
             Result.success(route.copy(
                 polylines = newPolylines,
                 distance = distance.roundToInt()
@@ -47,7 +43,7 @@ class GetPathToCarUseCase(
         var closestIndex = 0
 
         for (i in polylines.indices) {
-            val distance = calculateDistanceBetweenLocations(currentLocation, polylines[i])
+            val distance = distanceCalculator.calculateDistanceBetweenLocations(currentLocation, polylines[i])
             if (distance < minDistance) {
                 minDistance = distance
                 closestIndex = i
@@ -56,19 +52,4 @@ class GetPathToCarUseCase(
 
         return closestIndex
     }
-}
-
-private fun calculateDistanceBetweenLocations(
-    firstLocation: Location,
-    secondLocation: Location
-): Float {
-    val result = FloatArray(1)
-    android.location.Location.distanceBetween(
-        firstLocation.latitude,
-        firstLocation.longitude,
-        secondLocation.latitude,
-        secondLocation.longitude,
-        result
-    )
-    return result.first()
 }
